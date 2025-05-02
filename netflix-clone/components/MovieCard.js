cb5e542ee98927daeca9ed9ae31b5de837c21c08
Sakindu Dinsara <161465821+Loo0per@users.noreply.github.com>
2025-05-02 20:10:02 +0530

    Update MovieCard.js

--- netflix-clone/components/MovieCard.js
+++ netflix-clone/components/MovieCard.js
@@ -1,15 +1,13 @@
-import Image from 'next/image';
-
 export default function MovieCard({ movie }) {
   return (
     <div className="w-48 flex-shrink-0 transform transition duration-300 hover:scale-105">
-      <Image
+      <img
         src={movie.poster}
         alt={movie.title}
         width={192}
         height={288}
         className="rounded"
-        unoptimized
+        loading="lazy"
       />
       <p className="text-white mt-2 truncate">{movie.title}</p>
     </div>
